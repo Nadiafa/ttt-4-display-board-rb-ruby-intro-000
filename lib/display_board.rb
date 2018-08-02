1387793d72079e3d527d2adcb461e373f1d7ae46
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-02 15:59:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(array)
-  cell_row
+def display_board
+  cell_row = "   |   |   "
+  dash_row = "-----------"
+  puts cell_row
+  puts dash_row
+  puts cell_row
+  puts dash_row
+  puts cell_row
 end
